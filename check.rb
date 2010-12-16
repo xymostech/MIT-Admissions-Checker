e82ea038f0b9d3eda7c41d177ca978632f60ba33
Emily Eisenberg <xymostech@gmail.com>
2010-12-15 20:45:16 -0700

    checks for change on main page

--- check.rb
+++ check.rb
@@ -2,11 +2,31 @@ require 'httpclient'
 
 certs='./certs'
 
+nochange = true
+
+orig_redirectcontent = '0;URL=verify.php'
+orig_redirecturl = 'verify.php'
+
 # Check main page for changes
 
 client = HTTPClient.new()
 client.ssl_config.set_trust_ca(certs)
 
-uri='https://decisions.mit.edu/'
+home='https://decisions.mit.edu/'
 
-puts client.get_content(uri)
+homesource = client.get_content(home)
+
+homesource =~ /CONTENT="(\S+)"/i
+redirectcontent = $1
+redirectcontent =~ /URL=(\S+)/
+redirecturl = $1
+
+if(redirectcontent != orig_redirectcontent)
+  nochange = false
+  orig_redirectcontent = redirectcontent
+end
+
+if(redirecturl != orig_redirecturl)
+  nochange = false
+  orig_redirecturl = redirecturl
+end
